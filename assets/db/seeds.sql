45f1129f2115a4804c5e92b09aa539f12c2d8f1a
DamascusKraken <damascuskraken39@gmail.com>
2023-11-14 13:48:57 -0600

    Complete Seeds.sql file

--- assets/db/seeds.sql
+++ assets/db/seeds.sql
@@ -17,3 +17,17 @@ VALUES("Investment Broker", 116728.00),
       ("Customer Service Representative", 42733.00),
       ("Customer Service Manager", 98654.00);
 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES("Hareltila", "Frye", 10, NULL),
+      ("Tineth", "Vash", 7, 13),
+      ("Xylsea", "Dalaney", 2, NULL),
+      ("Charix", "Thorsten", 4),
+      ("Viokar", "Bryon", 9, 1),
+      ("Krovras", "Bates", 6, NULL),
+      ("Streghor", "Wise", 1, NULL),
+      ("Graexir", "Gulliver", 3, NULL),
+      ("Muror", "Peck", 9, 1),
+      ("Zebrum", "Tremain", 2, 3),
+      ("Līmos", "Rue", 1, 7),
+      ("Derabo", "Malik", 5, NULL),
+      ("Ethais", "Thayer", 8, NULL);
