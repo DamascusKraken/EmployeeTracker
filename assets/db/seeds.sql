860ab0a3df0dcb1ee688747d118203ed20d53225
DamascusKraken <damascuskraken39@gmail.com>
2023-11-14 12:52:21 -0600

    completed seeds for role table population

--- assets/db/seeds.sql
+++ assets/db/seeds.sql
@@ -3,6 +3,17 @@ VALUES("Asset Management"),
       ("Creative Services"),
       ("Engineering"),
       ("Legal"),
-      ("Quality Assurance"),
-      ("Customer Service"),
-      ("Finance / Accounting");
+      ("Customer Service"),;
+
+INSERT INTO role(role_title, salary)
+VALUES("Investment Broker", 116728.00),
+      ("Financial Advisor", 94170.00),
+      ("Graphic Designer", 50265.00),
+      ("Web Designer", 55078.00),
+      ("Software Engineer", 105590.00),
+      ("Computer Engineer", 114600.00),
+      ("Legal Assistant", 55199.00),
+      ("Complaince Manager", 145762.00),
+      ("Customer Service Representative", 42733.00),
+      ("Customer Service Manager", 98654.00);
+
